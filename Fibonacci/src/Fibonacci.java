aa4e6144031d818cdc15a553169ec7d7aafb9ee2
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2020-02-03 18:28:29 -0600

    Comentando
    termine el codigo y comento para enviar

--- Fibonacci/src/Fibonacci.java
+++ Fibonacci/src/Fibonacci.java
@@ -1,20 +1,24 @@
+//importamos la clase para poder leer el numero ingresado
 import java.util.Scanner;
 
 public class Fibonacci {
     public void inicio(){
+        //pedimos el ingreso de un numero
         System.out.println("Ingrese un numero: ");
         Scanner num = new Scanner(System.in);
         int a=num.nextInt();
-        fibo(a);
+        System.out.println("El numero ingresado es: "+a+" y su Fibonacci es: "+fibo(a));
     }
-    public void fibo(int x){
-        int f0=0;
-        int f1=1;        
-        int n = x;
-        int f=0;
-        
-        
-        
-        System.out.println("El numero ingresado es: "+x+" y su numero Fibonacci es: "+f);
+    public int fibo(int n){
+        if(n>1){
+            return fibo(n-1)+fibo(n-2);
+        }else if(n==1){
+            return 1;
+        }else if(n==0){
+            return 0;
+        }else{
+            System.out.println("El numero ingresado no es valido.");
+            return 0;
+        }
     }
 }
