a6907ef7e90e45481c579314e6aa72b95b8879e3
Jorge Argueta <34260059+Arg45@users.noreply.github.com>
2020-02-03 19:46:26 -0600

    fin de comentarios
    se termino el codigo

--- Fibonacci/src/Tarea3.java
+++ Fibonacci/src/Tarea3.java
@@ -2,9 +2,14 @@
  *
  * @author argue
  */
+
+//clase principal
 public class Tarea3 {
+    //metodo main
     public static void main(String[] args) {
+        //creamos variable de la otra clase para acceder a sus metodos
         Fibonacci f = new Fibonacci();
+        //accedemos al metodo deseado
         f.inicio();
     }  
 }
